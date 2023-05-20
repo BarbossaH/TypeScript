8b1c9b4142afbf35a42879c562727d56fd419cfe
Julian <huangbobeijing2008@gmail.com>
2023-05-21 00:15:22 +1200

    configure the typescript in tscconfig.json

---
+++ Basic520/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let username = 'Julian and Fiona and someone';
+console.log(username);
+let a = 12;
+let b = 6;
+// let b: number = '6';
+let c = 2;
+console.log(b / c);
+console.log(b * c * a);
---
+++ Basic520/src/main.ts
@@ -0,0 +1,9 @@
+let username = 'Julian and Fiona and someone';
+console.log(username);
+
+let a: number = 12;
+let b = 6;
+// let b: number = '6';
+let c: number = 2;
+console.log(b / c);
+console.log(b * c * a);
